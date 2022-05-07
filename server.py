b727d438d31017b9c21042c081ed31abf750542b
Omar <xxxomarxxx21@gail.com>
2022-05-07 07:07:34 +0200

    reimplemented in python

---
+++ server.py
@@ -0,0 +1,17 @@
+import socket
+
+if __name__ == "__main__":
+    ip = "127.0.0.1"
+    port = 1234
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((ip, port))
+    server.listen(5)
+
+    while True:
+        client, address = server.accept()
+        print(f"connection established - {address[0]}:{address[1]}")
+        string = client.recv(1024)
+        string = string.decode("utf-8")
+        client.send(bytes(string.upper(), "utf-8"))
+        client.close()
