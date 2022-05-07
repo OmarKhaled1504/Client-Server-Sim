292ce6470125c476af401c164a89772d5b884017
Omar <xxxomarxxx21@gail.com>
2022-05-07 11:20:45 +0200

    implemented Post requests and multithreading

--- client.py
+++ client.py
@@ -1,14 +1,30 @@
 import socket
 
 if __name__ == "__main__":
+
     ip = "127.0.0.1"
     port = 1234
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.connect((ip, port))
     with open('commands.txt') as file:
-        lines = file.readlines()
-        lines = [line.rstrip() for line in lines]
-    for line in lines:
-        server.send(bytes(line, "utf-8"))
-    message = server.recv(1024)
-    print(message.decode("utf-8"))
+        commands = file.readlines()
+        commands = [command.rstrip() for command in commands]
+    for command in commands:
+        command = command + f"\nHOST: {ip}"
+        filename = command.split()[1]
+        if command.split()[0] == "GET":
+            server.sendall(bytes(command, "utf-8"))
+            print('.')
+        elif command.split()[0] == "POST":
+            try:
+                f = open(f"client files/{filename}", mode='r')
+                data = f.read()
+                contentLength = "\nContent-Length: " + str(len(data)) + "\n\n"
+                request = command + contentLength + data
+                server.sendall(bytes(request, "utf-8"))
+            except IOError:
+                print('file not found.')
+        message = server.recv(4096)
+        print(message.decode("utf-8"))
+    server.close()
+# TODO http 1.1 & bonus & fix a bug in multithreading
--- server.py
+++ server.py
@@ -1,33 +1,56 @@
 import socket
+import threading
+from _thread import *
+
+print_lock = threading.Lock()
+
+
+def threaded(client):
+    # data received from c
+    while True:
+        request = client.recv(4096)
+        request = request.decode("utf-8")
+        request_type, filename = parse(request)
+        message = "HTTP/1.0 200 OK\r\n"
+        if request_type == "get":
+            try:
+                f = open(f"server files/{filename}", mode='r')
+                message = message + f.read() + "\r\n"
+            except IOError:
+                message = "HTTP/1.0 404 Not Found\r\n"
+            client.sendall(bytes(message, "utf-8"))
+        elif request_type == "post":
+            data = request.split()[-1]
+            print(f"HTTP/1.0 200 OK\r\ndata: {data}")
+            with open(f"client to server files/{request.split()[1]}", 'w') as f:
+                f.write(data)
+            client.sendall(bytes(message, "utf-8"))
+        client.close()
 
 
 def parse(request):
-    if request.split()[0] == 'GET':
-        filename = request.split()[1]
-        return "get", filename
-    elif request.split()[0] == 'POST':
-        return "post"
+    try:
+        if request.split()[0] == 'GET':
+            filename = request.split()[1]
+            return "get", filename
+        elif request.split()[0] == 'POST':
+            return "post", None
+    except:
+        return None, None
 
 
 if __name__ == "__main__":
     ip = "127.0.0.1"
     port = 1234
-
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((ip, port))
-    server.listen(5)
-
+    print("Server started")
+    print("socket binded to port", port)
+    print("Waiting for client request..")
+    server.listen()
     while True:
         client, address = server.accept()
-        print(f"connection established - {address[0]}:{address[1]}")
-        request = client.recv(1024)
-        request = request.decode("utf-8")
-        request_type, filename = parse(request)
-        if request_type == "get":
-            try:
-                f = open(f"server files/{filename}", mode='r')
-                message = f.read()
-            except IOError:
-                message = "HTTP/1.0 404 Not Found\r\n"
-            client.send(bytes(message, "utf-8"))
-        client.close()
+        print_lock.acquire()
+        start_new_thread(threaded, (client,))
+
+    server.close()
