caf65efee137437552496afd4ccf71f59519f905
Omar <xxxomarxxx21@gail.com>
2022-05-09 14:14:19 +0200

    implemented simple client side cache

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 from time import sleep
 
+cache = {}
 if __name__ == "__main__":
 
     with open('commands.txt') as file:
@@ -16,14 +17,23 @@ if __name__ == "__main__":
             server.connect((command.split()[5].split(':')[0], 80))
         filename = command.split()[1][1:]
         if command.split()[0] == "GET":
-            server.sendall(bytes(command, "utf-8"))
-            while True:
-                try:
-                    message = server.recv(4096)
-                    print(message.decode("utf-8"))
-                except socket.timeout:
-                    print("Server Timed Out\nConnection Closed")
-                    break
+            if filename in cache:
+                with open(cache[f'{filename}'], 'r') as f:
+                    print("file found in cache\n")
+                    print(f.read())
+            else:
+                print('file not found in cache...\nrequesting from server...')
+                server.sendall(bytes(command, "utf-8"))
+                while True:
+                    try:
+                        message = server.recv(4096)
+                        print(f'{message.decode("utf-8")}')
+                        with open(f"server to client files/{command.split()[1][1:]}", 'w') as f:
+                            f.write(message.decode("utf-8").split()[3])
+                            cache.update({command.split()[1][1:]: f.name})
+                    except socket.timeout:
+                        print("Server Timed Out\nConnection Closed")
+                        break
         elif command.split()[0] == "POST":
             try:
                 f = open(f"client files/{filename}", mode='r')
@@ -40,6 +50,6 @@ if __name__ == "__main__":
                 except socket.timeout:
                     print("Server Timed Out\nConnection Closed")
                     break
-        sleep(10)
+        sleep(6)
         server.close()
 # TODO bonus
