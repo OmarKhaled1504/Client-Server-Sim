fc79b0a417bed2efac4eff53b450568c65d7f61f
Omar <xxxomarxxx21@gail.com>
2022-05-13 18:14:57 +0200

    added support for .png files

--- client.py
+++ client.py
@@ -1,22 +1,25 @@
 import socket
 from time import sleep
+from PIL import Image
 
 cache = {}
 
 
 def compose(request_type, filename, host, port):
-    return f'{request_type} /{filename} HTTP/1.1\r\nHOST: {host}:{port}'
+    return f'{request_type} /{filename} HTTP/1.1\r\nHOST: {host}:{port}\r\n'
 
 
 if __name__ == "__main__":
-
-    with open('commands.txt') as file:
-        commands = file.readlines()
-        commands = [command.rstrip() for command in commands]
-
+    commands_file = input("Enter Commands File Name With Extension: ")
+    try:
+        with open(commands_file) as file:
+            commands = file.readlines()
+            commands = [command.rstrip() for command in commands]
+    except FileNotFoundError:
+        print("File Not Found!")
+        exit()
     for command in commands:
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.settimeout(2)
         ip = command.split()[2]
         try:
             server.connect((ip, int(command.split()[3][1:-1])))
@@ -30,45 +33,56 @@ if __name__ == "__main__":
             if filename in cache:
                 with open(cache[f'{filename}'], 'r') as f:
                     print("file found in cache\n")
-                    print(f.read())
+                    if filename.split(".")[1] == "txt":
+                        print(f.read())
+                    elif filename.split(".")[1] == "png":
+                        image = Image.open(cache[f'{filename}'])
+                        image.show()
             else:
                 print('file not found in cache...\nrequesting from server...')
                 server.sendall(bytes(request, "utf-8"))
-                try:
-                    if filename.split(".")[1] == "txt":
-                          message = server.recv(4096)
-                          print(f'{message.decode("utf-8")}')
-                          with open(f"server to client files/{command.split()[1][1:]}", 'w') as f:
-                              f.write(message.decode("utf-8").split()[3])
-                              cache.update({command.split()[1][1:]: f.name})
-                    elif filename.split(".")[1] == "png":
-                        with open(f"server to client files/{command.split()[1][1:]}", 'wb') as f:
-                          while True:
-                            message = server.recv(1024)
+
+                if filename.split(".")[1] == "txt":
+                    message = server.recv(4096)
+                    print(f'{message.decode("utf-8")}')
+                    with open(f"server to client files/{filename}", 'w') as f:
+                        f.write(message.decode("utf-8").split()[3])
+                        cache.update({filename: f.name})
+                elif filename.split(".")[1] == "png":
+                    with open(f"server to client files/{filename}", 'wb') as f:
+                        cache.update({filename: f.name})
+                        while True:
+                            message = server.recv(4096)
                             if not message:
-                             break
+                                break
                             else:
-                             f.write(message)
-                    print("image sent successfully")
-                except socket.timeout:
-                    print("Server Timed Out\nConnection Closed")
-                    break
+                                f.write(message)
+                    print("image received successfully")
+                    image = Image.open(cache[f'{filename}'])
+                    image.show()
         elif command.split()[0] == "POST":
             request = compose('POST', filename, ip, port)
-            try:
-                f = open(f"client files/{filename}", mode='r')
-                data = f.read()
-                contentLength = "\nContent-Length: " + str(len(data)) + "\n\n"
-                request = request + contentLength + data
-                server.sendall(bytes(request, "utf-8"))
-            except IOError:
-                print('file not found.')
-            while True:
+            if filename.split(".")[1] == "txt":
                 try:
-                    message = server.recv(4096)
-                    print(message.decode("utf-8"))
-                except socket.timeout:
-                    print("Server Timed Out\nConnection Closed")
-                    break
-        sleep(6)
+                    f = open(f"client files/{filename}", mode='r')
+                    data = f.read()
+                    contentLength = "\nContent-Length: " + str(len(data)) + "\n\n"
+                    request = request + contentLength + data
+                    server.sendall(bytes(request, "utf-8"))
+                except IOError:
+                    print('file not found.')
+            elif filename.split(".")[1] == "png":
+                try:
+                    f = open(f"client files/{filename}", mode='rb')
+                except IOError:
+                    print(f'file ({filename}) not found!')
+                    continue
+                server.send(bytes(request, 'utf-8'))
+                img = f.read(1024)
+                while img:
+                    server.send(img)
+                    img = f.read(1024)
+            message = server.recv(4096)
+            print(message.decode("utf-8"))
+
         server.close()
--- server.py
+++ server.py
@@ -9,42 +9,52 @@ def threaded(client):
     while True:
         try:
             request = client.recv(4096)
+            request = str(request)[2:-1]
             if not request:
                 print('Connection closed')
                 print_lock.release()
                 break
-            print(request)
         except socket.timeout:
             print_lock.release()
             client.close()
             print("Operation Timed Out\nConnection Closed")
             break
-        request = request.decode("utf-8")
 
         request_type, filename = parse(request)
-        message = "HTTP/1.0 200 OK\r\n"
-        if request_type == "get" and filename.split(".")[1] == "txt":
-            try:
-                f = open(f"server files/{filename}", mode='r')
-                message = message + f.read() + "\r\n"
-            except IOError:
-                message = "HTTP/1.0 404 Not Found\r\n"
-            client.sendall(bytes(message, "utf-8"))
+        reply = "HTTP/1.0 200 OK\r\n"
+        if request_type == "get":
+            if filename.split(".")[1] == "txt":
+                try:
+                    f = open(f"server files/{filename}", mode='r')
+                    reply = reply + f.read() + "\r\n"
+                except IOError:
+                    reply = "HTTP/1.0 404 Not Found\r\n"
+                client.sendall(bytes(reply, "utf-8"))
+            elif filename.split(".")[1] == "png":
+                f = open(f"server files/{filename}", mode='rb')
+                img = f.read(1024)
+                while img:
+                    client.send(img)
+                    img = f.read(1024)
         elif request_type == "post":
-            data = request.split()[-1]
-            print(f"data: {data}")
-            with open(f"client to server files/{request.split()[1][1:]}", 'w') as f:
-                f.write(data)
-            client.sendall(bytes(message, "utf-8"))
-        elif request_type == "get" and filename.split(".")[1] == "png":
-            f = open(f"server files/{filename}", mode='rb')
-            l = f.read(1024)
-            while l:
-                client.send(l)
-                l = f.read(1024)
-
-        #elif request_type == "post" and filename.split(".")[1] == "png":
-    client.close()
+            if filename.split(".")[1] == "txt":
+                data = request.split()[-1]
+                print(f"data: {data}")
+                with open(f"client to server files/{request.split()[1][1:]}", 'w') as f:
+                    f.write(data)
+                client.sendall(bytes(reply, "utf-8"))
+            elif filename.split(".")[1] == "png":
+                with open(f"client to server files/{filename}", 'wb') as f:
+                    while True:
+                        try:
+                            message = client.recv(4096)
+                        except socket.timeout:
+                            break
+                        if not message:
+                            break
+                        else:
+                            f.write(message)
+                client.sendall(bytes(reply, "utf-8"))
 
 
 def parse(request):
@@ -53,7 +63,8 @@ def parse(request):
             filename = request.split()[1][1:]
             return "get", filename
         elif request.split()[0] == 'POST':
-            return "post", None
+            filename = request.split()[1][1:]
+            return "post", filename
     except IndexError:
         return None, None
 
@@ -69,6 +80,6 @@ if __name__ == "__main__":
     server.listen()
     while True:
         client, address = server.accept()
-        client.settimeout(10)
+        client.settimeout(5)
         print_lock.acquire()
         start_new_thread(threaded, (client,))
